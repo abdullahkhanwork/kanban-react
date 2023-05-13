2cc70e70acf7bb6b77eb6a5752cdf744721a6c49
Abdullah Khan <abdullahkhanwork@gmail.com>
2023-05-13 13:06:18 +0500

    install fontawesome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
     return (
         <div className="App">
             <h1 className="text-3xl font-bold underline mt-7">
                 Hello world!
+                <FontAwesomeIcon icon={faFacebook}/>
             </h1>
         </div>
     );
